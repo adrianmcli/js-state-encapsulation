f45eaf7f3b5333a29c385a147740079449e52e61
Adrian Li <li.adrianmc@gmail.com>
2019-01-13 09:32:39 -0500

    minor refactor

--- 05_combine_reducers.js
+++ 05_combine_reducers.js
@@ -1,19 +1,17 @@
 const todoReducer = (state = [], action = {}) => {
   const actions = {
-    ADD_TODO: [...state, action.text]
+    ADD_TODO: [...state, action.text],
   };
   const newState = actions[action.type];
   return newState !== undefined ? newState : state;
 };
 
-const combineReducers = reducers => {
-  return (state = {}, action) => {
-    const newState = {}
-    Object.entries(reducers).forEach(([stateKey, reducerFn]) => {
-      newState[stateKey] = reducerFn(state[stateKey], action)
-    })
-    return newState
-  };
+const combineReducers = reducers => (state = {}, action) => {
+  const newState = {};
+  Object.entries(reducers).forEach(([stateKey, reducerFn]) => {
+    newState[stateKey] = reducerFn(state[stateKey], action);
+  });
+  return newState;
 };
 
 module.exports = { todoReducer, combineReducers };
